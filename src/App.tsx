31ffbb01189f4d4dd43c6b7eb59149049a576d33
Edmundo Abreu e Lima <engedmundo@gmail.com>
2023-12-27 15:43:57 -0300

    create app with ts

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>react app com ts</h1>
+    </>
+  )
+}
+
+export default App
