4fd7e74b04de80646fc4feb41f0ddcf0213e1f3f
Edmundo Abreu e Lima <engedmundo@gmail.com>
2023-12-30 10:15:35 -0300

    feature/improve auth methods

--- src/api/AuthService.tsx
+++ src/api/AuthService.tsx
@@ -9,8 +9,13 @@ interface AuthResponse {
   refreshToken?: string;
 }
 
+interface AuthHeaders {
+  Authorization: string;
+  'Content-Type': string;
+}
+
 class AuthApiService {
-  static async login(username: string, password: string): Promise<AuthResponse> {
+  public static async login(username: string, password: string): Promise<AuthResponse> {
     try {
       const url = `${BASE_URL}/token/`;
       const response = await axios.post(url, { username, password });
@@ -26,7 +31,6 @@ class AuthApiService {
     } catch (error) {
       if (axios.isAxiosError(error) && error.response) {
         const responseError: AxiosResponse = error.response;
-        console.log(responseError.data.detail);
         const errorMessage = responseError.data.detail;
 
         switch (responseError.status) {
@@ -42,63 +46,80 @@ class AuthApiService {
           message: errorMessage,
         }
       }
-      console.log('Não é um erro do axios', error);
-      throw new Error("Não é um erro do axios");
+      console.error('Erro inesperado', error);
+      throw new Error('Erro inesperado ao processar a autenticação');
     }
   }
 
-  static logout() {
+  public static logout(): void {
     sessionStorage.removeItem("username");
     sessionStorage.removeItem("accessToken");
     sessionStorage.removeItem("refreshToken");
+    localStorage.removeItem("username");
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
     return
   }
 
-  // static async checkAcessTokenValidity() {
-  //   try {
-  //     const accessToken = localStorage.getItem("accessToken");
-  //     const url = `${process.env.REACT_APP_API_BASE_URL}/token/verify/`;
-  //     const response = await axios.post(url, { token: accessToken });
-  //     return response.status;
-  //   } catch (error) {
-  //     if (error.response.status === 401) {
-  //       try {
-  //         const refreshResponse = this.refreshAccessToken();
-  //         return refreshResponse.status;
-  //       } catch (error) {
-  //         console.error('Falha ao atualizar access token', error);
-  //         return 401;
-  //       }
-  //     }
-  //     console.error('Erro desconhecido:', error);
-  //     return 500;
-  //   }
-  // }
+  public static async getHeaders(accessToken: string, refreshToken: string): Promise<AuthHeaders> {
+    const validityResponse = await this.checkAccessTokenValidity(accessToken);
 
-  // static async refreshAccessToken() {
-  //   try {
-  //     const refreshToken = localStorage.getItem("refreshToken");
-  //     const url = `${process.env.REACT_APP_API_BASE_URL}/token/refresh/`;
-  //     const response = await axios.post(url, { refresh: refreshToken });
-  //     const { access } = response.data;
-  //     localStorage.setItem("accessToken", access);
-  //     return response.status;
+    if (validityResponse.code !== 200) {
+      try {
+        const refreshResponse = await this.refreshAccessToken(refreshToken);
+        return {
+          'Authorization': `Bearer ${refreshResponse.accessToken}`,
+          'Content-Type': 'application/json',
+        }
+      } catch (error) {
+        console.error('Falha ao obter accessToken após atualização', error);
+        throw new Error('Falha ao obter accessToken após atualização');
+      }
+    }
+    return {
+      'Authorization': `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+    }
+  }
 
-  //   } catch (error) {
-  //     if (error.response.status === 401 || error.response.status === 400) {
-  //       return error.response.status;
-  //     }
-  //     console.error('Erro desconhecido:', error);
-  //     return 500;
-  //   }
-  // }
+  private static async checkAccessTokenValidity(accessToken: string): Promise<AuthResponse> {
+    try {
+      const url = `${BASE_URL}/token/verify/`;
+      const response = await axios.post(url, { token: accessToken });
+      return {
+        code: response.status,
+      };
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        const responseError: AxiosResponse = error.response;
+        return {
+          code: responseError.status,
+          message: responseError.data.detail
+        }
+      }
+      console.error('Access | Erro inesperado', error);
+      throw new Error('Erro inesperado ao processar a autenticação');
+    }
+  }
 
-  // static mountHeaders() {
-  //   return {
-  //     'Authorization': `Bearer ${localStorage.accessToken}`,
-  //     'Content-Type': 'application/json',
-  //   }
-  // }
+  static async refreshAccessToken(refreshToken: string): Promise<AuthResponse> {
+    try {
+      const url = `${BASE_URL}/token/refresh/`;
+      const response = await axios.post(url, { refresh: refreshToken });
+      const accessToken = response.data;
+      sessionStorage.setItem("accessToken", accessToken);
+      return {
+        code: response.status,
+        accessToken,
+      };
+
+    } catch (error) {
+      console.error('Erro durante a atualização do token de acesso', error);
+      throw new Error('Falha ao atualizar o token de acesso');
+    }
+  }
+
+
 }
 
 export default AuthApiService;
